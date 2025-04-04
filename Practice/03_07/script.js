93ed1a72505d6163add17d3329e1f4032b8d069f
Ryan M <44005846+Rylytics@users.noreply.github.com>
2025-04-03 22:36:12 -0400

    worked on Practice

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,28 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+const bag_of_holding = {
+  size: "medium",
+  color: "black",
+  weight: 10,
+  potion: {
+    type: "healing",
+    potency: 50,
+    quantity: 3,
+  },
+};
+
+const longsword = {
+  name: "Longsword of Fire",
+  base_damage: "1d8",
+  base_damage_type: "slashing",
+  modifier: "strength",
+  enchantment: {
+    additional_damage_type: "fire",
+    additional_damage: 2,
+  },
+};
+
+console.log(bag_of_holding); // Access the entire bag_of_holding object
+console.log(longsword);
